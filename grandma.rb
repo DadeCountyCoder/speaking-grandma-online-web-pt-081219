7b4e24d9c4a2f15560bc9af263a8604b490fb2c7
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-16 05:07:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 input_phrase = "Hi Nana, how are you?"
 def speak_to_grandma(phrase)  
   
-  # Check if string equals same phrase all upper case letters, which means string is all uppercase
+
   if phrase == phrase.upcase 
     # return this string if condition is true 
     puts "NO, NOT SINCE 1938!"
