0b8d1d67889c5208c72b567b07aae3cf493c7fa1
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-16 05:11:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(phrase)
-  
-  
+          
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+
     if phrase != phrase.upcase()
       return "HUH?! SPEAK UP, SONNY!"
     else
