042a6a407c89321b3011d53218f2c65c6a26a484
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-16 04:26:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 
-describe '#speak_to_grandma' do	def speak_to_grandma(phrase)
+def speak_to_grandma(phrase)
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do	    if phrase != phrase.upcase()
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'	      return "HUH?! SPEAK UP, SONNY!"
+
+(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'	      return "HUH?! SPEAK UP, SONNY!"
   end	    else
 
        return "NO, NOT SINCE 1938!"
