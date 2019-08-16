bec920d82b7292eb420d939898958a359eb7a1e6
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-16 04:54:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,12 +6,11 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
 
     
-    # elsif phrase == phrase.downcase
-  		# return "HUH?! SPEAK UP, SONNY!"
-    # elsif phrase == phrase.downcase
-    #   return "HUH?! SPEAK UP, SONNY!"
-    # elsif phrase == phrase.upcase
-    #   return "NO, NOT SINCE 1938!"
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+  
     # NO, NOT SINCE 1938!
     else
   		return "NO, NOT SINCE 1938!"
