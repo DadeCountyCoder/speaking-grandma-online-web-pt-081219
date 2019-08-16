cba8cadfa555151a1f1e1171b77c13b83d4fe604
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-16 05:09:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,9 @@
-input_phrase = "Hi Nana, how are you?"
-def speak_to_grandma(phrase)  
-  
-
-  if phrase == phrase.upcase 
-    # return this string if condition is true 
-    puts "NO, NOT SINCE 1938!"
-  else 
-    # return this string if condition is false 
-    puts "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+    if phrase != phrase.upcase()
+      return "HUH?! SPEAK UP, SONNY!"
+    else
+      return "NO, NOT SINCE 1938!"
+    end
 end
 
 
